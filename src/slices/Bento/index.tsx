12ca7f2a038c3ba05df09f1ce29100faef544d86
Akarsh Bajpai <onlyakarsh@gmail.com>
2024-04-29 23:05:19 +0530

    add bento headings

--- src/slices/Bento/index.tsx
+++ src/slices/Bento/index.tsx
@@ -1,5 +1,7 @@
-import { Content } from "@prismicio/client";
-import { SliceComponentProps } from "@prismicio/react";
+import Bounded from "@/components/Bounded";
+import { Content, asText } from "@prismicio/client";
+import { PrismicNextImage } from "@prismicio/next";
+import { PrismicRichText, SliceComponentProps } from "@prismicio/react";
 
 /**
  * Props for `Bento`.
@@ -11,12 +13,36 @@ export type BentoProps = SliceComponentProps<Content.BentoSlice>;
  */
 const Bento = ({ slice }: BentoProps): JSX.Element => {
   return (
-    <section
+    <Bounded
       data-slice-type={slice.slice_type}
       data-slice-variation={slice.variation}
     >
-      Placeholder component for bento (variation: {slice.variation}) Slices
-    </section>
+      <PrismicRichText field={slice.primary.heading}
+        components={{
+          heading2: ({ children }) => (
+            <h2 className="text-balance text-center text-5xl font-medium md:text-7xl">
+              {children}
+            </h2>
+          ),
+          em: ({ children }) => (
+            <em className="bg-gradient-to-b from-yellow-100 to-yellow-500 bg-clip-text not-italic text-transparent">
+              {children}
+            </em>
+          ),
+        }}
+      />
+
+      <div className="mx-auto mt-6 max-w-md text-balance text-center text-slate-300"></div>
+      <PrismicRichText field={slice.primary.body} />
+
+      {slice.items.map((item) => (
+        <div key={asText(item.title)} className="glass-container">
+          <PrismicRichText field={item.title} />
+          <PrismicRichText field={item.body} />
+          <PrismicNextImage field={item.image} />
+        </div>
+      ))}
+    </Bounded>
   );
 };
 
