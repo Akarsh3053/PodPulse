ca8b588ae700780bc9fb8d67a0d4b87f47f3005f
Akarsh Bajpai <onlyakarsh@gmail.com>
2024-04-29 22:20:44 +0530

    hero slice completed

--- src/slices/Hero/index.tsx
+++ src/slices/Hero/index.tsx
@@ -18,18 +18,29 @@ const Hero = ({ slice }: HeroProps): JSX.Element => {
     <Bounded
       data-slice-type={slice.slice_type}
       data-slice-variation={slice.variation}
+      className="text-center"
     >
       <div className="realtive">
         {/* <StarGrid /> */}
         {isFilled.richText(slice.primary.heading) && (
-          <h1 className="text-balance text-center text-5xl font-medium md:text-7xl">
-            <PrismicText field={slice.primary.heading} />
+          <h1 className="text-balance text-5xl font-medium md:text-7xl">
+            <PrismicRichText field={slice.primary.heading} />
           </h1>
         )}
-        <PrismicRichText field={slice.primary.heading} />
-        <PrismicRichText field={slice.primary.body} />
-        <ButtonLink field={slice.primary.button}>{slice.primary.button_label}</ButtonLink>
-        <PrismicNextImage field={slice.primary.image} />
+        {isFilled.richText(slice.primary.body) && (
+          <div className="mx-auto mt-6 max-w-md text-balance text-slate-300">
+            <PrismicRichText field={slice.primary.body} />
+          </div>
+        )}
+        {isFilled.link(slice.primary.button) && (
+          <ButtonLink className="mt-8" field={slice.primary.button}>{slice.primary.button_label}</ButtonLink>
+        )}
+        {isFilled.image(slice.primary.image) && (
+          <div className="glass-container mt-16 w-fit">
+            <div className="absolute inset-0 -z-10 bg-blue-500/30 blur-2xl filter" />
+            <PrismicNextImage className="rounded-lg" field={slice.primary.image} />
+          </div>
+        )}
       </div>
     </Bounded>
   );
