c362fc348a912206c059e7879c85c1040620896d
Akarsh Bajpai <onlyakarsh@gmail.com>
2024-07-15 23:07:33 +0530

    add sidebar

---
+++ components/LeftSidebar.tsx
@@ -0,0 +1,16 @@
+import Image from "next/image"
+import Link from "next/link"
+
+const LeftSidebar = () => {
+    return (
+        <section className="left_sidebar">
+            <nav className="flex flex-col gap-6">
+                <Link href="/">
+                    <Image src="" />
+                </Link>
+            </nav>
+        </section>
+    )
+}
+
+export default LeftSidebar
