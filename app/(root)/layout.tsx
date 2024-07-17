cc9f5e09878234f3d12c5a6e90cea85e19ea8972
Akarsh Bajpai <onlyakarsh@gmail.com>
2024-07-17 19:33:46 +0530

    fix layout

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,4 +1,6 @@
 import LeftSidebar from "@/components/LeftSidebar";
+import RightSidebar from "@/components/RightSidebar";
+import Image from "next/image";
 
 export default function RootLayout({
     children,
@@ -6,12 +8,23 @@ export default function RootLayout({
     children: React.ReactNode;
 }>) {
     return (
-        <div>
-            <main>
+        <div className="relative flex flex-col">
+            <main className="relative flex bg-black-3">
                 <LeftSidebar />
-                {children}
-                <p className="text-white-1">RIGHT SIDEBAR</p>
 
+                <section className="border-2 border-red-500 flex min-h-screen flex-1 flex-col px-4 sm:px-14">
+                    <div className="mx-auto flex w-full max-w-5xl flex-col max-sm:px-4">
+                        <div>
+                            {/* <Image src="/" alt="" /> */}
+                            MobileNav
+                        </div>
+                        <div>
+                            Toaster pos
+                            {children}
+                        </div>
+                    </div>
+                </section>
+                <RightSidebar />
             </main>
         </div>
     );
--- components/LeftSidebar.tsx
+++ components/LeftSidebar.tsx
@@ -30,7 +30,9 @@ const LeftSidebar = () => {
 
                     return <Link href={route}
                         key={label}
-                        className={cn("flex gap-3 items-center py-4 max-lg:px-4 justify-center lg:justify-start",)}
+                        className={cn("flex gap-3 items-center py-4 max-lg:px-4 justify-center lg:justify-start", {
+                            'bg-nav-focus border-r-4 border-orange-1': isActive
+                        })}
                     >
                         <Image src={imgURL} alt={label} width={24} height={24} />
                         <p>{label}</p>
