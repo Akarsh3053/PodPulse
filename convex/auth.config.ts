0edb47a300f60c699fe233727a682e93cf81f556
Akarsh Bajpai <onlyakarsh@gmail.com>
2024-07-22 22:35:05 +0530

    add new config

---
+++ convex/auth.config.ts
@@ -0,0 +1,10 @@
+const authConfig = {
+    providers: [
+        {
+            domain: "https://assured-grubworm-9.clerk.accounts.dev",
+            applicationID: "convex",
+        },
+    ]
+};
+
+export default authConfig
