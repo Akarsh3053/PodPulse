5ed4ae006c99c813f3a95177ffd38baba0b61466
Akarsh Bajpai <onlyakarsh@gmail.com>
2024-08-18 00:06:13 +0530

    complete the upload widget

--- components/GenerateThumbnail.tsx
+++ components/GenerateThumbnail.tsx
@@ -79,12 +79,15 @@ const GenerateThumbnail = ({ setImage, setImageStorageId, image, imagePrompt, se
                             height={40}
                         />
                     ) : (
-                        <div>
+                        <div className="text-16 text-center font-medium text-white-1">
                             Uploading
                             <Loader size={20} className="animate-spin ml-2" />
                         </div>
                     )}
-
+                    <div className="flex flex-col items-center gap-1">
+                        <h2 className="text-12 font-bold text-orange-1">Click to upload</h2>
+                        <p className="text-12 font-normal text-gray-1">SVG, PNG, JPG or GIF (max, 1080x1080px)</p>
+                    </div>
                 </div>
             )}
         </>
