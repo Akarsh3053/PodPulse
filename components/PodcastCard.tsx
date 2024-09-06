d406db0944e6ce08bff8c29c0512fb42a598be83
Akarsh Bajpai <onlyakarsh@gmail.com>
2024-09-06 15:38:16 +0200

    Add changes

--- components/PodcastCard.tsx
+++ components/PodcastCard.tsx
@@ -1,14 +1,21 @@
 import { PodcastCardProps } from '@/types'
 import Image from 'next/image'
+import { useRouter } from 'next/navigation'
 import React from 'react'
 
 const PodcastCard = ({ title, description, podcastId, imgUrl }: PodcastCardProps) => {
 
+    const router = useRouter()
+
     const handleViews = () => {
-        // TODO: Implement view count tracking
+        // TODO: Increase views
+
+        router.push(`/podcast/${podcastId}`, {
+            scroll: true
+        })
     }
     return (
-        <div className='cursor-pointer'>
+        <div className='cursor-pointer' onClick={handleViews}>
             <figure className='flex flex-col gap-2'>
                 <Image
                     src={imgUrl}
