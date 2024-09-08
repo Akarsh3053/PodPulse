b1e02c287c9456d683aa0ad99753cf4f37c95b95
Akarsh Bajpai <onlyakarsh@gmail.com>
2024-09-08 13:25:16 +0200

    Fixed next config

--- next.config.mjs
+++ next.config.mjs
@@ -7,16 +7,12 @@ const nextConfig = {
     remotePatterns: [
       {
         protocol: 'https',
-        hostname: 'lovely-flamingo-139.convex.cloud'
-      },
-      {
-        protocol: 'https',
-        hostname: 'sleek-capybara-771.convex.cloud'
-      },
-      {
-        protocol: 'https',
         hostname: 'img.clerk.com'
       },
+      {
+        protocol: 'https',
+        hostname: 'next-terrier-191.convex.cloud'
+      }
     ]
   }
 };
